6291faf393d0a502561b62518b4a48c7074d9b23
didier-tp <didier@ici-ou-la.fr>
2019-02-06 15:20:03 +0100

    interface ProduitService

---
+++ src/main/java/com/atos/tp/core/ProduitService.java
@@ -0,0 +1,8 @@
+package com.atos.tp.core;
+
+import java.util.List;
+
+public interface ProduitService {
+	public List<Produit> produitsSelonCategorie(String categorie);
+	public List<String> getAllCategories();
+}
